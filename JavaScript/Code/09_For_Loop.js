155ed5aaa2b0c023dc3d22f3a51b0bfe59ca4a3c
Shashwat <ssri.gzp@gmail.com>
2023-08-14 20:23:57 +0530

    use prompt in vscode

--- JavaScript/Code/09_For_Loop.js
+++ JavaScript/Code/09_For_Loop.js
@@ -1,4 +1,5 @@
 // Find the sum of first n natural number
+// You have to write this every time in program to use prompt in vs code
 const prompt = require("prompt-sync")({ sigint: true});
 let sum = 0;
 let n = prompt('Enter any natural number');
